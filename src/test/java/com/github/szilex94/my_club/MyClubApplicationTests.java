e1e650742eafb3ba8978b178465f8d87ba85f05b
szilard.mate <szilard.mate@treedev.site>
2021-02-01 20:16:16 +0200

    MC-1 empty Spring boot project

---
+++ src/test/java/com/github/szilex94/my_club/MyClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.szilex94.my_club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
